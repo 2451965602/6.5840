02250359334a6aba5b3b5b66377c78880fc27932
Lin Tengyue <83867465+2451965602@users.noreply.github.com>
2024-08-14 17:21:42 +0800

    Update util.go

--- src/raft/util.go
+++ src/raft/util.go
@@ -10,3 +10,32 @@ func DPrintf(format string, a ...interface{}) {
 		log.Printf(format, a...)
 	}
 }
+
+func (rf *Raft) GetNextIndex(i int) int {
+	if rf.lastIncludedIndex == -1 {
+		return rf.nextIndex[i]
+	}
+	return rf.nextIndex[i] - rf.lastIncludedIndex - 1
+}
+
+func (rf *Raft) GetLogsLen() int {
+	return rf.lastIncludedIndex + len(rf.logs)
+}
+
+func (rf *Raft) GetCommitIndex() int {
+	return rf.lastIncludedIndex + len(rf.logs)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
